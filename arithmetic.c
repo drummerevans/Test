aa9fc50495fd39ad8be9f41c3bc9ff4ac0a8ec9c
drummerevans <drummerevans@live.co.uk>
2018-09-16 12:26:04 +0100

    readme.md included

--- arithmetic.c
+++ arithmetic.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 int addition(int x, int y);
+int subtraction(int x, int y);
 
 // This is the main function
 int main() {
-    int a, b;
-    a = 20;
+    int a, b; 
+    a = 20; 
     b = 10;
 
     printf("%d + %d = %d\n", a, b, addition(a, b));
+    printf("%d - %d = %d\n", a, b, subtraction(a, b));
 
     return 0;
 }
 
 int addition(int x, int y) {
     return x + y;
+}
+
+int subtraction(int x, int y) {
+    return x - y;
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+## arithmetic.c
+
+This program executes the addition and subtraction of two integers.
+
+To run:
+gcc arithmetic.c -o arithmetic.exe
+./arithmetic.exe
